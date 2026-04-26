34db76aede9290dc9d4e557d9d37315a586ca447
Prak005 <prakharmallick005@gmail.com>
2026-04-26 18:10:11 +0530

    add database pool

---
+++ db/pool.js
@@ -0,0 +1,14 @@
+const { Pool } = require('pg');
+
+const connectionString = 
+    process.env.DATABASE_URL ||
+    `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
+const pool = new Pool({
+    connectionString,
+    ssl: process.env.DATABASE_URL
+        ? { rejectUnauthorized: false }
+        : false,
+});
+
+module.exports = pool;
